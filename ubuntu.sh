dfe77351b4021d29873078eef88f822a6122d177
Ardang666 <ardang2015@gmail.com>
2018-09-17 20:35:37 +0800

    [Modify] Change TsingHua src for docker.
    Signed-off-by: Ardang666 <ardang2015@gmail.com>

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,6 +1,5 @@
-sudo apt update
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs)  stable"
-sudo apt update
-sudo apt install docker-ce
+sudo apt-get update
+sudo apt-get install docker-ce

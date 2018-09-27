0921fed977ece82c4ce1ce9972f68d70400a5a18
Ardang666 <ardang2015@163.com>
2018-09-27 16:15:01 +0800

    [add] add bazel cfg.
    Signed-off-by: Ardang666 <ardang2015@163.com>

--- cfg_bazel.sh
+++ cfg_bazel.sh
@@ -1,7 +1,10 @@
-echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
+sudo apt-get install openjdk-8-jdk
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update && sudo apt-get install oracle-java8-installer
 
+echo "deb [arch=amd64] http://storage.googleapis.com/bazel-apt stable jdk1.8" | sudo tee /etc/apt/sources.list.d/bazel.list
 curl https://bazel.build/bazel-release.pub.gpg | sudo apt-key add -
+
 sudo apt-get update && sudo apt-get install bazel
 sudo apt-get upgrade bazel
 
-#sudo ./bazel-0.16.1-installer-linux-x86_64.sh

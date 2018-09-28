ea09aa5684b1ca9f5d90a7f93ad13334b1aea059
Ardang666 <ardang2015@163.com>
2018-09-28 15:58:15 +0800

    [Modify] fix bug for samba
    Signed-off-by: Ardang666 <ardang2015@163.com>

--- cfg_samba.sh
+++ cfg_samba.sh
@@ -2,4 +2,6 @@ sudo apt install -y samba
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
 sudo cp ./smb.conf /etc/samba/smb.conf
 sudo service smbd restart
+
+sudo useradd user
 sudo smbpasswd -a user

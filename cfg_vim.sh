72e7e108609b058523cf73abe27b8f151e1bd643
Ardang666 <ardang2015@163.com>
2018-09-30 16:07:27 +0800

    [Fix] vim for -y
    Signed-off-by: Ardang666 <ardang2015@163.com>

--- cfg_vim.sh
+++ cfg_vim.sh
@@ -1,11 +1,11 @@
-sudo apt install vim vim-scripts vim-gtk vim-gnome
+sudo apt install -y vim vim-scripts vim-gtk vim-gnome
 
 # vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # YCM
 # https://www.v2ex.com/t/341751
-sudo apt install llvm-3.9 clang-3.9 libclang-3.9-dev libboost-all-dev
-sudo apt install cmake
+sudo apt install -y llvm-3.9 clang-3.9 libclang-3.9-dev libboost-all-dev
+sudo apt install -y cmake
 #git clone --recursive https://github.com/Valloric/YouCompleteMe.git && git submodule update --init --recursive
 
 mkdir ~/.ycm_build
@@ -18,7 +18,7 @@ rm ~/.ycm_build -rf
 
 # ctags
 # https://www.cnblogs.com/zjutzz/p/9393397.html
-sudo apt install autoconf
+sudo apt -y install autoconf
 cd /tmp
 git clone https://github.com/universal-ctags/ctags
 cd ctags
@@ -30,10 +30,10 @@ sudo make install
 sudo ln -s /opt/software/universal-ctags/bin/ctags /usr/bin/ctags
 
 # cscope
-sudo apt install cscope
+sudo apt -y install cscope
 
 # powerline
-sudo apt install powerline
+sudo apt -y install powerline
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
 wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbols.conf
 mv PowerlineSymbols.otf ~/.local/share/fonts/
@@ -41,10 +41,8 @@ fc-cache -vf ~/.local/share/fonts/
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 
 cp ~/.vimrc ~/.vimrc.bak.$DATA
-cp ~/.vimrc.base ~/.vimrc.base.bak.$DATA
-cp ~/.vimrc.vundels ~/.vimrc.vundels.bak.$DATA
+mv ~/.vimself ~/.vimself
 
 cp .vimrc ~/.vimrc
-cp .vimrc.base ~/.vimrc.base
-cp .vimrc.vundles ~/.vimrc.vundles
+cp -r .vimself ~/.vimself
 

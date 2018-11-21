8c5f1dd32a10d11746bb0c97e04500b9db139b60
Ardang666 <ardang2015@gmail.com>
2018-11-21 11:36:46 +0800

    [fix] ycm for vim
    Signed-off-by: Ardang666 <ardang2015@gmail.com>

--- cfg_vim.sh
+++ cfg_vim.sh
@@ -1,24 +1,26 @@
 export pwd=${PWD}
 pushd ${pwd}
 	sudo apt install -y vim vim-scripts vim-gtk vim-gnome
-
 	# vundle
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 	# YCM
 	# https://www.v2ex.com/t/341751
 	sudo apt install -y llvm-3.9 clang-3.9 libclang-3.9-dev libboost-all-dev
 	sudo apt install -y cmake
-	git clone --recursive https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe && \
-	 cd ~/.vim/bundle/YouCompleteMe && \
-	 git submodule update --init --recursive
+	git clone --recursive https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe \
+	  && cd ~/.vim/bundle/YouCompleteMe \
+	  && git submodule update --init --recursive
 
 	mkdir ~/.ycm_build
 	cd ~/.ycm_build
-
-	cmake -G "Unix Makefiles" -DUSE_SYSTEM_BOOST=ON -DEXTERNAL_LIBCLANG_PATH=/usr/lib/x86_64-linux-gnu/libclang-3.9.so . ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
+	cmake -G "Unix Makefiles" -DUSE_SYSTEM_BOOST=ON -DEXTERNAL_LIBCLANG_PATH=/usr/lib/x86_64-linux-gnu/libclang-3.9.so . \
+    ~/.vim/bundle/YouCompleteMe/third_party/ycmd/cpp
 	cmake --build . --target ycm_core --config Release
+  cd ~/.vim/bundle/YouCompleteMe
+  ./install.py --clang-completer 
 	cp ~/.vim/bundle/YouCompleteMe/third_party/ycmd/examples/.ycm_extra_conf.py ~/.vim/
 	rm ~/.ycm_build -rf
+  cd $pwd
 
 	# ctags
 	sudo apt -y install exuberant-ctags

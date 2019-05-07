400210d2d52df417aa6fd74470c47440294e7a98
Ardang666 <ardang2015@gmail.com>
2019-05-07 16:47:54 +0800

    add dl_env

---
+++ cfg_dl_env.sh
@@ -0,0 +1,9 @@
+sudo apt install -y git
+sudo apt install -y python-dev
+sudo apt install -y protobuf-c-compiler protobuf-compiler
+sudo apt install -y libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev 
+sudo apt install -y libatlas-base-dev
+sudo apt install -y libgoogle-glog-dev liblmdb-dev
+sudo apt install -y libgflags-dev
+sudo apt install -y libopenblas-dev
+sudo apt install -y --no-install-recommends libboost-all-dev
